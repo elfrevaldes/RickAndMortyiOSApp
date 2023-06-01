02799bd84817191fa0c1835ad01a96a5d571a42f
Elfre Valdes <EValdes@verisk.com>
2023-06-01 16:06:22 -0600

    Simple request using RMRequest

--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -13,7 +13,15 @@ final class RMCharacterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        let request = RMRequest(
+            endPoint: .character,
+            //pathComponents: ["1"],
+            queryParams: [
+                URLQueryItem(name: "name", value: "rick"),
+                URLQueryItem(name: "status", value: "alive")
+            ]
+        )
+        print(request.url)
     }
     init(title: String) {
         super.init(nibName: nil, bundle: nil)
